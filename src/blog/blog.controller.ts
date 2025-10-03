7be288c05335f6aa3d850ad9010e0ab189bef58a
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-10-03 12:18:17 +0500

    add blog controller with sample data and DTO, set global API prefix

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  blogs: BlogDto[];
+  constructor() {
+    this.blogs = [
+      {
+        id: 1,
+        title: 'First Blog',
+        content: 'This is the content of the first blog post.',
+      },
+      {
+        id: 2,
+        title: 'Second Blog',
+        content: 'This is the content of the second blog post.',
+      },
+      {
+        id: 3,
+        title: 'Third Blog',
+        content: 'This is the content of the third blog post.',
+      },
+      {
+        id: 4,
+        title: 'Fourth Blog',
+        content: 'This is the content of the fourth blog post.',
+      },
+    ];
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  async getBlog() {
+    return this.blogs;
+  }
+}
