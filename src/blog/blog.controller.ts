27e1e549d0ca72f64f521081156375297e59e920
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-10-04 12:21:00 +0500

    add CRUD operations to blog controller

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BlogDto } from './dto/blog.dto';
 
 @Controller('blog')
 export class BlogController {
   blogs: BlogDto[];
+
   constructor() {
     this.blogs = [
       {
@@ -33,5 +45,51 @@ export class BlogController {
   @Get()
   async getBlog() {
     return this.blogs;
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  async createBlog(@Body() body: BlogDto) {
+    this.blogs.push({
+      id: this.blogs[this.blogs.length - 1].id + 1,
+      title: body.title,
+      content: body.content,
+    });
+    return this.blogs;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  async getBlogById(@Param('id') id: number) {
+    const blog = this.blogs.find((blog) => blog.id === Number(id));
+    if (!blog) {
+      throw new NotFoundException('Blog not found');
+    }
+    return blog;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  async updateBlog(@Param('id') id: number, @Body() body: BlogDto) {
+    const blogIndex = this.blogs.findIndex((blog) => blog.id === Number(id));
+    if (blogIndex === -1) {
+      throw new NotFoundException('Blog not found');
+    }
+    this.blogs[blogIndex] = {
+      id: Number(id),
+      title: body.title,
+      content: body.content,
+    };
+    return this.blogs[blogIndex];
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  async deleteBlog(@Param('id') id: number) {
+    const blogIndex = this.blogs.findIndex((blog) => blog.id === Number(id));
+    if (blogIndex === -1) {
+      throw new NotFoundException('Blog not found');
+    }
+    this.blogs.splice(blogIndex, 1);
   }
 }
